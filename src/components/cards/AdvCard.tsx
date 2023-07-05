dc202041dcb79e706482cfb2c9afaaeec5cba651
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-07-05 17:16:03 +0700

    fixed: links-and hover

--- src/components/cards/AdvCard.tsx
+++ src/components/cards/AdvCard.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import styles from './AdvCard.module.scss';
 import { IAdvCard } from '../../models/types';
 import { Link } from 'react-scroll';
-
 interface AdvCardProps {
     item: IAdvCard;
 }
 
+// - [x] Ссылка (обычная и подчеркнутая пунктиром) при наведении должны менять цвет на #6d8601
 const AdvCard: React.FC<AdvCardProps> = ({ item }) => {
+    console.log(item.href);
+
     return (
         <div className={styles.element}>
             <img className={styles.img} src={item.img} alt={item.alt} />
-            <Link spy={true} smooth={true} duration={1000} key={item.title} to={item.href} className={styles.link}>
-                {item.title}
-            </Link>
+            {item.href ? (
+                <Link spy={true} smooth={true} duration={1000} key={item.title} to={item.href} className={styles.link}>
+                    {item.title}
+                </Link>
+            ) : (
+                <div className={styles.description}>{item.title}</div>
+            )}
         </div>
     );
 };
--- src/components/slides/feedback/Feedback.tsx
+++ src/components/slides/feedback/Feedback.tsx
@@ -12,32 +12,35 @@ const Feedback = () => {
     const { feedbackSlides } = config;
 
     return (
-        <Swiper
-            className={styles.element}
-            spaceBetween={30}
-            centeredSlides={true}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            pagination={{
-                clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            // отладочная печать
-            onSlideChange={() => console.log('Feedback -> slide change')}
-            //onSwiper={(swiper) => console.log(swiper)}
-        >
-            {feedbackSlides.items.map((slide, index) => (
-                <SwiperSlide key={index}>
-                    <div className={styles.element}>
-                        <p className={styles.title}>{slide.title}</p>
-                        <p>{slide.description}</p>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <div id="feedback">
+            <h2> {feedbackSlides.title}</h2>
+            <Swiper
+                className={styles.element}
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                // отладочная печать
+                // onSlideChange={() => console.log('Feedback -> slide change')}
+                //onSwiper={(swiper) => console.log(swiper)}
+            >
+                {feedbackSlides.items.map((slide, index) => (
+                    <SwiperSlide key={index}>
+                        <div className={styles.element}>
+                            <p className={styles.title}>{slide.title}</p>
+                            <p>{slide.description}</p>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
     );
 };
 
